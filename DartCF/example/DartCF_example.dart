ee54c5743d016a2ad6e9b9ceaedd02e3c53ce32b
aduii <alexjuep@hotmail.com>
2020-05-25 18:48:29 -0500

    DartCF

---
+++ DartCF/example/DartCF_example.dart
@@ -0,0 +1,6 @@
+import 'package:DartCF/DartCF.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ DartCF/lib/DartCF.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library DartCF;
+
+export 'src/DartCF_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
